10b0da70507c9b93a848b2b1f0d4c7483678c9bd
Trần Nhật Linh <trannhatlinh120801@gmail.com>
2021-09-15 22:31:40 +0700

    a + b

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("haha");
+        int a = 1; 
+        int b = 2; 
+        System.out.println(a+b);
     }
 }
