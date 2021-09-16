9a062634a6cc81409cc78a7301b0b78f05164b43
Trần Nhật Linh <trannhatlinh120801@gmail.com>
2021-09-16 21:54:13 +0700

    them ham cong

--- src/App.java
+++ src/App.java
@@ -1,8 +1,13 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("haha");
         int a = 1; 
         int b = 2; 
         System.out.println(a+b);
+        Sum test = new Sum(a,b);
+        System.out.println(test.tong());
+
+        
     }
 }
---
+++ src/Sum.java
@@ -0,0 +1,12 @@
+public class Sum {
+    int a; 
+    int b;
+    Sum(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    
+    int tong() {
+        return a+b;
+    }
+}
