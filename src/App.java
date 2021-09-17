19a237e5a3e9b078eb32d43bb200e9e98e82abd9
Tran Anh Tuan <superman61088@gmail.com>
2021-09-17 22:53:02 +0700

    in^2

--- src/App.java
+++ src/App.java
@@ -2,13 +2,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("haha");
-        int a = 1; 
-        int b = 2; 
+        int a = 1;
+        int b = 2;
         int c = 3;
-        System.out.println(a+b);
-        Sum test = new Sum(a,b);
-        Sum test1 = new Sum(a,b,c); 
+        System.out.println(a + b);
+        Sum test = new Sum(a, b);
+        Sum test1 = new Sum(a, b, c);
         System.out.println(test.tong());
         System.out.println(test1.tongba());
+        System.out.println(test.nhan());
+        System.out.println(test1.nhanba());
+        System.out.println(test1.binhphuong());
+
     }
 }
