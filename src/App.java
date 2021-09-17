f6472f7a6e62d20597179d44bbc5635b33497c07
Tran Anh Tuan <superman61088@gmail.com>
2021-09-17 23:05:38 +0700

    hinhchunhant

--- src/App.java
+++ src/App.java
@@ -5,14 +5,18 @@ public class App {
         int a = 1;
         int b = 2;
         int c = 3;
+        double length = 5.0;
+        double width = 3.3;
         System.out.println(a + b);
         Sum test = new Sum(a, b);
         Sum test1 = new Sum(a, b, c);
+        Sum test2 = new Sum(length, width);
         System.out.println(test.tong());
         System.out.println(test1.tongba());
         System.out.println(test.nhan());
         System.out.println(test1.nhanba());
         System.out.println(test1.binhphuong());
-
+        System.out.println(test2.Area());
+        System.out.println(test2.Perimeter());
     }
 }
--- src/Sum.java
+++ src/Sum.java
@@ -1,29 +1,51 @@
 public class Sum {
-    private int a; 
+    private double length;
+    private double width;
+    private int a;
     private int b;
     private int c;
+
+    Sum(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    double Area() {
+        return length * width;
+    }
+
+    double Perimeter() {
+        return length + width;
+    }
+
     Sum(int a, int b) {
         this.a = a;
         this.b = b;
     }
-    int nhan() { 
-        return a*b;
+
+    int nhan() {
+        return a * b;
     }
+
     int tong() {
-        return a+b;
+        return a + b;
     }
-    Sum(int a, int b,int c){
+
+    Sum(int a, int b, int c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
-    int tongba(){
+
+    int tongba() {
         return tong() + c;
     }
-    int nhanba(){
-        return a*b*c;
+
+    int nhanba() {
+        return a * b * c;
     }
-    int binhphuong(){
-        return a*a +b*b;
+
+    int binhphuong() {
+        return a * a + b * b;
     }
 }
